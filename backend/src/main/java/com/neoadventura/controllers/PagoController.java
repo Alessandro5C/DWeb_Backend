38119726f424fc58f3c14f0f97301cdb65de8c2a
Yefri <u201912400@upc.edu.pe>
2021-06-29 10:17:35 -0500

    Bug solucionado del cross origin
    Al final del trabajo, cambiamos la dirección del cross origin a la dirección del netlify

--- backend/src/main/java/com/neoadventura/controllers/PagoController.java
+++ backend/src/main/java/com/neoadventura/controllers/PagoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping(path= "/neo-adventura"+"/v1")
 public class PagoController {
 
